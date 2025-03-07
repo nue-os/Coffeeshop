9ed3bcc627f017a95e84b41d7df893ac4abfdc6f
nue-os <kimsoeun0108@gmail.com>
2025-03-07 13:31:20 +0900

    feat: 첫 구조

---
+++ src/main/java/com/shop/cafe/controller/ProductController.java
@@ -0,0 +1,41 @@
+package com.shop.cafe.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shop.cafe.dto.Product;
+import com.shop.cafe.service.ProductService;
+
+@RestController
+@CrossOrigin("http://127.0.0.1:5500/")
+public class ProductController {
+
+	@Autowired
+	ProductService productService;
+
+	Map<String, Object> storage = new HashMap<>();
+
+	@GetMapping("getAllProducts")
+	public List<Product> getAllProducts() {
+		try {
+			// 처음에 가져온 데이터가 있으면 다시 가져오지 않고 그대로 보여주기
+			Object o = storage.get("firstPageProducts"); 
+			if (o == null) {
+				List<Product> list = productService.getAllProducts();
+				storage.put("firstPageProducts", list);
+				return list;
+			}
+			return (List<Product>) o;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+}
